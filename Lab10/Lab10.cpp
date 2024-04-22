742dccf37fdb6eac6535f4a151540dbd1870029c
Боев Андрей Олегович <aoboev@edu.hse.ru>
2024-04-22 13:32:40 +0300

    added files

--- Lab10/Lab10.cpp
+++ Lab10/Lab10.cpp
@@ -4,23 +4,51 @@
 using namespace std;
 
 struct student {
-    struct student* next;
+    struct student* next = NULL;
     struct { string first, last, patronymic; } name;
     string group;
-    struct { int B[5] = {0}; float average; } grades;
+    struct { int B[5] = { 0 }; float average; } grades;
 };
 
+struct student* read_fl();
+void input_student(student* student);
+bool insert_student(struct student** lst, struct student* new_student, string group);
+void stack_to_file(struct student* lst);
+void print_stack(struct student* lst);
+string read_str();
+void free_memory(student* lst);
+
+
+int main()
+{
+    struct student** lst = new student*;
+    *lst = read_fl();
+    print_stack(*lst);
+
+    string group;
+    cout << "Input the group number: " << endl;
+    group = read_str();
+    student* new_student = new student;
+    input_student(new_student);
+    if (insert_student(lst, new_student, group))
+        print_stack(*lst);
+    else
+        cout << "No students with the " << group << " group were found." << endl;
+    stack_to_file(*lst);
+    free_memory(*lst);
+    return 0;
+}
 
 struct student* read_fl() {
-    int i = 1; // счётчик полей в структуре 
-    struct student *lst = NULL, *curr = new student;
+    int i = 1;
+    struct student* lst = NULL, * curr = new student;
     string str;
 
     string filename = ".txt";
-    /*cout << "Input name of the file with data: " << endl;
+    cout << "Input name of the file with data: " << endl;
     filename = read_str();
     cout << endl;
-    */
+
     fstream fl;
     fl.open(filename);
     if (!fl) {
@@ -31,35 +59,33 @@ struct student* read_fl() {
         getline(fl, str);
         switch (i)
         {
-            case 1:
-                curr->name.last = str;
-                break;
-            case 2:
-                curr->name.first = str;
-                break;
-            case 3:
-                curr->name.patronymic = str;
-                break;
-            case 4:
-                curr->group = str;
-                break;
-            case 5:
-                int j = 0;
-                for (int k = 0; k < str.length(); k++) {
-                    if (str[k] == ',')
-                        continue;
-                    if (str[k] == ' ') {
-                        j++;
-                    }
-                    else {
-                        curr->grades.B[j] = curr->grades.B[j] * 10 + (str[k] - 48);
-                    }
-                }
-                break;  
+        case 1:
+            curr->name.last = str;
+            break;
+        case 2:
+            curr->name.first = str;
+            break;
+        case 3:
+            curr->name.patronymic = str;
+            break;
+        case 4:
+            curr->group = str;
+            break;
+        case 5:
+            int j = 0;
+            for (int k = 0; k < str.length(); k++) {
+                if (str[k] == ',')
+                    continue;
+                if (str[k] == ' ')
+                    j++;
+                else
+                    curr->grades.B[j] = curr->grades.B[j] * 10 + (str[k] - 48);
+            }
+            break;
         }
         if (i == 5) {
             i = 1;
-            curr->grades.average = (float)(curr->grades.B[0] + curr->grades.B[1] + 
+            curr->grades.average = (float)(curr->grades.B[0] + curr->grades.B[1] +
                 curr->grades.B[2] + curr->grades.B[3] + curr->grades.B[4]) / 5;
             curr->next = lst;
             lst = curr;
@@ -71,34 +97,7 @@ struct student* read_fl() {
     return lst;
 }
 
-string read_str()
-{
-    string s = "";
-    do
-    {
-        getline(cin, s);
-    } while (!s.size());
-
-    return s;
-}
-
-void print_stack(struct student* lst) {
-    struct student* p = lst;
-    while (p) {
-        cout << "Last name:     " << p->name.last << endl;
-        cout << "First name:    " << p->name.first << endl;
-        cout << "Patronymic:    " << p->name.patronymic << endl;
-        cout << "Group:         " << p->group << endl;
-        cout << "Grades:        ";
-        for (int i = 0; i < 5; ++i)
-            cout << p->grades.B[i] << " ";
-        cout << endl;
-        cout << "GPA:           " << p->grades.average << endl << endl;
-        p = p->next;
-    }
-}
-
-void input_student(student *student) {
+void input_student(student* student) {
     cout << "Last name:     ";
     student->name.last = read_str();
     cout << "First name:    ";
@@ -124,24 +123,94 @@ void input_student(student *student) {
         student->grades.B[2] + student->grades.B[3] + student->grades.B[4]) / 5;
 }
 
-bool insert_student(student *lst, student *new_student) {
+bool insert_student(struct student** lst, struct student* new_student, string group) {
     bool flag = false;
-
+    struct student* curr, * pred;
+    curr = pred = *lst;
+    while (curr)
+    {
+        if (curr->group == group)
+        {
+            struct student* temp = new student;
+            temp->name = new_student->name;
+            temp->group = new_student->group;
+            temp->grades = new_student->grades;
+            if (curr == pred) {
+                temp->next = *lst;
+                *lst = temp;
+            }
+            else {
+                temp->next = curr;
+                pred->next = temp;
+            }
+            flag = true;
+        }
+        pred = curr;
+        curr = curr->next;
+    }
     return flag;
 }
 
-int main()
+void stack_to_file(struct student* lst) {
+    struct student* p = lst;
+    ofstream fo;
+    string filename_out;
+    cout << "Input name of the file with results: " << endl;
+    filename_out = read_str();
+    cout << endl;
+    if (filename_out.substr(filename_out.length() - 4) != ".txt") {
+        cout << "Wrong filename. " << endl;
+        exit(1);
+    }
+    else {
+        fo.open(filename_out);
+        while (p) {
+            fo << p->name.last << endl;
+            fo << p->name.first << endl;
+            fo << p->name.patronymic << endl;
+            fo << p->group << endl;
+            for (int i = 0; i < 4; ++i)
+                fo << p->grades.B[i] << " ";
+            fo << endl << p->grades.average << endl << endl;
+            p = p->next;
+        }
+    }
+}
+
+string read_str()
 {
-    student *lst;
-    lst = read_fl();
-    print_stack(lst);
+    string s = "";
+    do
+    {
+        getline(cin, s);
+    } while (!s.size());
 
-    string group;
-    cout << "Input the group number: " << endl;
-    group = read_str();
-    student *new_student = new student;
-    input_student(new_student);
+    return s;
+}
 
+void print_stack(struct student* lst) {
+    cout << endl;
+    struct student* p = lst;
+    while (p) {
+        cout << "Last name:     " << p->name.last << endl;
+        cout << "First name:    " << p->name.first << endl;
+        cout << "Patronymic:    " << p->name.patronymic << endl;
+        cout << "Group:         " << p->group << endl;
+        cout << "Grades:        ";
+        for (int i = 0; i < 5; ++i)
+            cout << p->grades.B[i] << " ";
+        cout << endl;
+        cout << "GPA:           " << p->grades.average << endl << endl;
+        p = p->next;
+    }
+}
 
-    return 0;
+void free_memory(student* lst) {
+    student* p = lst, * next = lst;
+    while (p) {
+        next = p->next;
+        delete p;
+        p = next;
+    }
+    cout << "The memory is cleared." << endl;
 }
